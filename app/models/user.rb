f5e897c21a895049ead6d6e1ffd987021b4f55ac
Quang Tran <quang.tran@suse.com>
2019-11-08 15:09:15 +0100

    commented code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
   has_many :messages
+  #regular expression:
+  #w: matches any word character (alpanumeric&underscore)
+  #+: match 1 or more of the preceeding token
   NAME_REGEX = /\w+/
-  validates :username, presence: true, uniqueness: { case_sensitive: false },
-                       format: { with: /\A#{NAME_REGEX}\z/i },
-                       length: { maximum: 15 }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :username, presence: true,
+    uniqueness: { case_sensitive: false },
+
+    #Regular expression
+    #//i i-modifier perform case insestive matching
+    #don't know what /\\w+\z/i mean
+    format: { with: /\A#{NAME_REGEX}\z/i },
+    length: { maximum: 15 }
+  #password must be at least 6 characters long
+  validates :password, presence: true,
+    length: { minimum: 6 }
   has_secure_password
 end
