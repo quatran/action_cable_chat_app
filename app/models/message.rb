9ad5500b34f770a849fbe20bc0a7c6f7146ae1e6
Quang Tran <quang.tran@suse.com>
2019-11-08 15:12:14 +0100

    commented message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
+  
+  #display most recent last at a maximum at 50
   scope :for_display, -> { order(:created_at).last(50) }
 end
